278cf690a7ab052ee8304a55c1d19b081aa72ccf
Aldair Raul Cosetito Coral <aldair.soft@hotmail.com>
2021-10-27 16:32:16 -0500

    Add .gitignore file

---
+++ DeliveryFoodUI/DeliveryFoodUI/DeliveryFoodUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryFoodUIApp.swift
+//  DeliveryFoodUI
+//
+//  Created by Aldair Raul Cosetito Coral on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryFoodUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
