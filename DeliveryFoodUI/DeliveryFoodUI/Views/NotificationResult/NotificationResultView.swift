4699dea44cf3b2e2202359c801edf3fa74d6d5e9
Aldair Raul Cosetito Coral <aldair.soft@hotmail.com>
2021-11-08 11:54:33 -0500

    feature/notificationResult
    start doing notification result view.

---
+++ DeliveryFoodUI/DeliveryFoodUI/Views/NotificationResult/NotificationResultView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationResultView.swift
+//  DeliveryFoodUI
+//
+//  Created by Aldair Raul Cosetito Coral on 8/11/21.
+//
+
+import SwiftUI
+
+struct NotificationResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationResultView()
+    }
+}
