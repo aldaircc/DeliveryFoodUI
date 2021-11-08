98160f65b79432c1b83cc24d33d00bac8b509463
Aldair Raul Cosetito Coral <aldair.soft@hotmail.com>
2021-11-08 12:08:39 -0500

    feature/congrats notification
    view complete!

--- DeliveryFoodUI/DeliveryFoodUI/Views/NotificationResult/NotificationResultView.swift
+++ DeliveryFoodUI/DeliveryFoodUI/Views/NotificationResult/NotificationResultView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct NotificationResultView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .center) {
+            VStack(alignment: .leading) {
+                Image("Pattern")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .opacity(0.56)
+                Spacer()
+            }
+            .ignoresSafeArea()
+            
+            VStack {
+                Image("notificationSuccess")
+                    .resizable()
+                    .frame(width: 172, height: 162)
+                    .padding(.bottom, 33)
+                VStack(spacing: 12) {
+                    Text("Congrats!")
+                        .font(.custom("BentonSans Bold", size: 30))
+                        .foregroundColor(.green)
+                    Text("Your Profile Is Ready To Use")
+                        .font(.custom("BentonSans Bold", size: 23))
+                    
+                }
+            }
+            
+            VStack {
+                Spacer()
+                Button(action: { }) {
+                    Text("Try Order")
+                        .font(.custom("BentonSans Bold", size: 16))
+                        .foregroundColor(.white)
+                }
+                .frame(width: 157, height: 57)
+                .background(Color.greenButton)
+                .cornerRadius(15.0)
+            }
+            .padding(.bottom, 60)
+            
+        }
+        
     }
 }
 
